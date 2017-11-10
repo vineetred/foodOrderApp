31e2bc6d81859b1f0b46dbff93850ea65e0960b8
Vineet <saivineetreddy.thatiparthi_ug19@ashoka.edu.in>
2017-11-10 22:52:55 +0530

    Bug fixes. Fixed client app

--- client/views.py
+++ client/views.py
@@ -6,7 +6,7 @@ from orderfood.models import hungercycle, foodjunction
 
 # Create your views here.
 def clientLogin(request):
-    return render(request, "login.html")
+    return render(request, "client/home.html")
 
 def clientAuth(request):
     data= request.POST      #getting post data
@@ -15,7 +15,21 @@ def clientAuth(request):
     user_authenticated = authenticate(username=username, password=password)     #inbuilt django function to try and authenticate user. user= None, if not authenticated
     if user_authenticated != None:   #if user is authenticated
         print("inside != none!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", request.user.username, request.user.get_username())
-        return redirect('/clientOrder')   #render the order-food page
+        return redirect('/client/order')   #render the order-food page
 
     else:  #if user is not authenticated
         return redirect('/client')   #go back to login page
+
+def order(request):
+    q = foodjunction.objects.values_list('order').filter(status="open").order_by('status')
+    return render(request, "client/order.html",{'q':q})
+
+def done(request):
+    data = request.POST.get("Order")
+    mark = data["Order"]
+    foodjunction.objects.filter(order =mark).update(status = 'close')
+    
+    return redirect('/client/order')
+
+
+ 
--- foodapp/settings.py
+++ foodapp/settings.py
@@ -72,7 +72,9 @@ ROOT_URLCONF = 'foodapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -138,3 +140,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/orderfood'
+LOGOUT_REDIRECT_URL = '/'
