e3c7ae104cfef53aea0c617b1aa7059a68c9c24b
Vineet Reddy <saivineetreddy.thatiparthi_ug19@ashoka.edu.in>
2017-12-09 20:18:58 +0530

    Created a model call profile in accounts/models.py
    Working towards an email confirmation service.

--- accounts/models.py
+++ accounts/models.py
@@ -3,14 +3,12 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-
-#CREATED A MODEL THAT TAKES CARE OF THE PROFILE! Uses signals to achieve this.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
-#<---THIS SIGNAL STARTS BELOW THIS!--->
+
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
     if created:
