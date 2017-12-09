986d7a2614a82fb6807e08890ccc54482466e1d6
Vineet Reddy <saivineetreddy.thatiparthi_ug19@ashoka.edu.in>
2017-12-09 21:42:58 +0530

    Added email confirmation!
    YAAY! Added email confirmation. Works currently with the inbuilt Django email server. Should be changed later.

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,8 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    birth_date = forms.DateField(help_text='Required. Format: YYYY-MM-DD')
-
+    email = forms.EmailField(max_length=200, help_text='Required')
     class Meta:
         model = User
-        fields = ('username', 'birth_date', 'password1', 'password2', )
+        fields = ('username', 'email', 'password1', 'password2')
