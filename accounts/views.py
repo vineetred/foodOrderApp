43bbb5b090d0d3ac7749baae989c95bf21c46db2
vidursingh <vidur.singh_ug19@ashoka.edu.in>
2017-12-09 22:56:00 +0530

    databse changes

--- accounts/views.py
+++ accounts/views.py
@@ -47,6 +47,7 @@ def activate(request, uidb64, token):
         user.save()
         login(request, user)
         # return redirect('home')
-        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+        return render (request, 'redirect.html')
+        #return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
     else:
         return HttpResponse('Activation link is invalid!')
