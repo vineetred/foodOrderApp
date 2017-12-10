bab157972c5025592e673d3c645c396dca670587
vidursingh <vidur.singh_ug19@ashoka.edu.in>
2017-12-10 11:33:53 +0530

    edited format of order history

--- orderfood/models.py
+++ orderfood/models.py
@@ -12,7 +12,7 @@ class hungercycle(models.Model):
     status = models.CharField(max_length=100)
 
     def __str__(self):    #describes what to do if print statement is used with item of this class
-        return(self.name+ self.order)
+        return(self.name + "---:  " + self.order)
 
 class hungercycle_menu(models.Model):
     foodItem = models.CharField(max_length = 300) #Item name
@@ -25,7 +25,7 @@ class foodjunction(models.Model):
     order= models.CharField(max_length=300)
     status= models.CharField(max_length=100)
     def __str__(self):
-        return(self.name+ self.order)
+        return(self.name+"---:  "+ self.order)
 
 class foodjunction_menu(models.Model):
     foodItem = models.CharField(max_length = 300) #Item name
