9223b3207da1056c0ada762d5c19a7fed1590aaa
vidursingh <vidur.singh_ug19@ashoka.edu.in>
2017-12-10 09:31:45 +0530

    order history complete
    open orders and closed orders are now implemented.

--- orderhistory/views.py
+++ orderhistory/views.py
@@ -9,8 +9,14 @@ from orderfood.models import hungercycle, foodjunction
 
 def orderHistory(request):
     username= request.user.username
-    orderListFoodjunction= foodjunction.objects.all().filter(name= username)
-    orderListHungercycle = hungercycle.objects.all().filter(name=username)
+    orderListFoodjunction_open= foodjunction.objects.all().filter(name= username).filter(status= "open")
+    orderListFoodjunction_close= foodjunction.objects.all().filter(name= username).filter(status= "close")
+    orderListHungercycle_open = hungercycle.objects.all().filter(name=username).filter(status= "open")
+    orderListHungercycle_close = hungercycle.objects.all().filter(name=username).filter(status= "close")
+
     #for i in range(len(orderList)):
     #    orderList[i]= str(orderList[i]).strip(username)
-    return render(request, "orderHistory.html", {'orderListFoodjunction': orderListFoodjunction,'orderListHungercycle': orderListHungercycle})
+    return render(request, "orderHistory.html", {'orderListFoodjunction_open': orderListFoodjunction_open,
+                                                 'orderListFoodjunction_close': orderListFoodjunction_close,
+                                                 'orderListHungercycle_close': orderListHungercycle_close,
+                                                 'orderListHungercycle_open': orderListHungercycle_open})
